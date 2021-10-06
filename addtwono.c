4619a7bbe5926d82d9aacc30d55105c1eaa72b87
Tripti <er.jaintripti@gmail.com>
2021-10-06 14:57:23 +0530

    first copy of my file

---
+++ addtwono.c
@@ -0,0 +1,16 @@
+//add two numbers
+
+#include<stdio.h>
+
+void addTwo()
+{
+	int n1,n2;
+	printf("Enter two integers");
+	scanf("%d,%d",&n1,&n2);
+	printf("Sum of the two number is %d",n1+n2);	
+}
+
+void main()
+{
+	
+}
